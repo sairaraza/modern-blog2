df28d15ca1e3d36145d01f4dbf65c3cc726cf4e1
sraza <saira.raza@iaac.net>
2019-11-23 18:49:17 +0000

    t

--- js/card-modern-blog.js
+++ js/card-modern-blog.js
@@ -58,8 +58,8 @@ var Card = (function (window) {
         // Compose sequence and use duration to overlap tweens.
         this._TL.add(slideContentDown);
         this._TL.add(clipImageIn, 0);
-        this._TL.add(floatContainer/*, '-=' + clipImageIn.duration() * 0.1*/);
-        this._TL.add(clipImageOut/*, '-=' + floatContainer.duration() * 0.1*/);
+        this._TL.add(floatContainer, '-=' + clipImageIn.duration() * 0.3);
+        this._TL.add(clipImageOut, '-=' + floatContainer.duration() * 0.3);
         this._TL.add(slideContentUp/*, '-=' + clipImageOut.duration() * 0.6*/);
 
         this.isOpen = true;
@@ -73,7 +73,7 @@ var Card = (function (window) {
      */
     Card.prototype._slideContentDown = function () {
 
-        var tween = TweenLite.to(this._content, 0.6, {
+        var tween = TweenLite.to(this._content, 0.8, {
             y: window.innerHeight,
             ease: Expo.easeInOut
         });
